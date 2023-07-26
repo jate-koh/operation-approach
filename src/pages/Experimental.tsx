316618216e0e81abffe640ae3c91c3c46db834cb
jate-koh <jate_ben@hotmail.com>
2023-07-26 15:56:58 +0700

    `feat`: add operation group

--- src/pages/Experimental.tsx
+++ src/pages/Experimental.tsx
@@ -5,7 +5,6 @@ import { randomId } from '@/utils/String';
 // Dependencies Import
 import React from 'react';
 import RGL, { WidthProvider, Layout, Responsive } from 'react-grid-layout';
-
 // Test Props
 function generateLayout() {
     // Create a layout with Random number of elements
@@ -44,24 +43,100 @@ export function Experimental() {
     let counter: number = 0;
 
     return (
-        <div className=''>
+
+        <div className='flex flex-col'>
+            <div
+                className="bg-white w-[10rem] h-[5rem]"
+                draggable={true}
+                // this is a hack for firefox
+                // Firefox requires some kind of initialization
+                // which we can do by adding this attribute
+                // @see https://bugzilla.mozilla.org/show_bug.cgi?id=568313
+                onDragStart={e => e.dataTransfer.setData("text/plain", "")}
+                data-grid={{x: 0, y: 0, w: 1, h: 1}}
+            >
+                Droppable Element
+            </div>
             <GridLayout
                 isResizable={false} isDraggable={true}
+                isDroppable={true} droppingItem={layoutState.draggingItem}
                 compactType={'vertical'} measureBeforeMount={true}
                 rowHeight={64} draggableHandle='.drag-handle'
-                onDrag={(layout, oldItem, newItem) => {
+                // onDrag={(layout, oldItem, newItem) => {
+                //     setLayoutState({
+                //         ...layoutState,
+                //         dragging: true,
+                //         draggingItem: newItem,
+                //     });
+                //     console.log('X', newItem.x, 'Y', newItem.y);
+                // }}
+                onDragStart={(layout, oldItem, newItem) => {
                     setLayoutState({
                         ...layoutState,
                         dragging: true,
                         draggingItem: newItem,
                     });
-                    console.log('X', newItem.x, 'Y', newItem.y);
+                }}
+                onDragStop={(layout, oldItem, newItem) => {
+                    setLayoutState({
+                        ...layoutState,
+                        dragging: false,
+                        draggingItem: undefined,
+                    });
+                }}
+                onDrop={(layout, dropItem) => {
+                    console.log('Dropped', layout, dropItem);
                 }}
                 cols={layoutState.cols}
             >
                 { layoutState.layout?.map((item) => {
                     return (
                         <div key={`item-${item.i}`} 
+                            draggable={true}
+                            className='bg-red-500 drag-handle'
+                            data-grid={{x: item.x, y: item.y, w: item.w, h: item.h, i: item.i}}
+                        >
+                            {counter++}
+                        </div>
+                    );
+                })}
+            </GridLayout>
+            <GridLayout
+                isResizable={false} isDraggable={true}
+                isDroppable={true} droppingItem={layoutState.draggingItem}
+                compactType={'vertical'} measureBeforeMount={true}
+                rowHeight={64} draggableHandle='.drag-handle'
+                // onDrag={(layout, oldItem, newItem) => {
+                //     setLayoutState({
+                //         ...layoutState,
+                //         dragging: true,
+                //         draggingItem: newItem,
+                //     });
+                //     console.log('X', newItem.x, 'Y', newItem.y);
+                // }}
+                onDragStart={(layout, oldItem, newItem) => {
+                    setLayoutState({
+                        ...layoutState,
+                        dragging: true,
+                        draggingItem: newItem,
+                    });
+                }}
+                onDragStop={(layout, oldItem, newItem) => {
+                    setLayoutState({
+                        ...layoutState,
+                        dragging: false,
+                        draggingItem: undefined,
+                    });
+                }}
+                onDrop={(layout, oldItem, newItem) => {
+                    console.log('Dropped');
+                }}
+                cols={layoutState.cols}
+            >
+                { layoutState.layout?.map((item) => {
+                    return (
+                        <div key={`item-${item.i}`} 
+                            draggable={true}
                             className='bg-red-500 drag-handle'
                             data-grid={{x: item.x, y: item.y, w: item.w, h: item.h, i: item.i}}
                         >
--- src/pages/PreviewPage.tsx
+++ src/pages/PreviewPage.tsx
@@ -1,14 +1,35 @@
 // File Imports
 import OperationFlow from '@/components/operation-flow/OperationFlow';
-import OperationFlowGrid from '@/components/operation-grid-demo/OperationFlowGrid';
+import OperationGroup from '@/components/operation-flow/OperationGroup';
+
+import { ColumnRegionConfigs, OperationGroupProps } from '@/components/operation-flow/utils/types/Props';
 
 // Props Imports
-import { testProps } from '@/pages/TestProps';
+import { generateProps } from '@/pages/Demo';
+import { testProps } from '@/pages/Test';
+
+const domainProps: OperationGroupProps = generateProps(1, 15);
+const regionProps: ColumnRegionConfigs = [
+    {   
+        regionNum: 0,
+        regionName: 'Prepare Provision',
+        regionDescription: '30 days' //number
+    },
+    {
+        regionNum: 1,
+        regionName: 'Raiding',
+        regionDescription: '15 - 20 days' //number
+    },
+];
 
 export function PreviewPage() {
     return (
         <div className='flex justify-center items-center min-h-screen'>
+            {/* <OperationFlow {...testProps} />
             <OperationFlow {...testProps} />
+            <OperationFlow {...testProps} /> */}
+
+            <OperationGroup {...domainProps} {...regionProps} />
         </div>
     );
 }
